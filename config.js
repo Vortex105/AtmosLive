960f285ad840cd7fbd8072d051b415e12909fa39
Hunsa-wusu Seseshe <seseshe14@gmail.com>
2025-04-09 18:55:16 +0100

    Restored Api keys

---
+++ config.js
@@ -0,0 +1,6 @@
+const myApiKey = 'b01788a528b0edc1cf85583a25d5e0ff';
+
+const UNSPLASH_ACCESS_KEY = 'qsOBkjrrFU1p5YStN5P2yaYz138M4Rz2FN8Hgtx6wOY';
+
+export { myApiKey };
+export { UNSPLASH_ACCESS_KEY };
