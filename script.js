ee4e1200e315c9f65c94b8d4fe8591fae587e581
Hunsa-wusu Seseshe <seseshe14@gmail.com>
2025-04-09 18:55:16 +0100

    Restored Api keys

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const myApiKey = process.env.MY_API_KEY;
-const unsplashAccessKey = process.env.UNSPLASH_ACCESS_KEY;
+import { myApiKey } from './config.js';
+import { UNSPLASH_ACCESS_KEY } from './config.js';
 
 window.addEventListener('DOMContentLoaded', () => {
 	// Optional: Clear only if data is outdated
